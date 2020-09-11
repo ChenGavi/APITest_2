0bb95dc0bd50f3d15e27cd3c7ea92d84b7c3853a
Gavin Chen <chen.gavin@outlook.com>
2020-09-10 22:22:29 -0400

    update

---
+++ src/test/java/com/automation/APITestFramework/utilities/TestConfig.java
@@ -0,0 +1,15 @@
+package com.automation.APITestFramework.utilities;
+
+public class TestConfig {
+
+    public static String server = "smtp.gmail.com";
+    public static String from = "pcchanger@gmail.com";
+    public static String password = "Qmj73GYM_CYAN";
+    public static String[] to = {"pcchanger@gmail.com"};
+    public static String subject = "Extent project Report";
+
+    public static String messageBody = "TestMessage";
+
+}
+
+
